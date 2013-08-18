affdb1846788fde98a050be783f146142bfcbd1a
axe3939 <jlc293@student.cpit.ac.nz>
2013-08-18 20:46:47 +1200

    Theseus and Minotaur github beginning

---
+++ TheseusAndMinotaur/TheseusAndMinotaur/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheseusAndMinotaur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Theseus");
+            Console.ReadLine();
+            Console.WriteLine("I am Minotaur");
+            Console.ReadLine();
+        }
+    }
+}
